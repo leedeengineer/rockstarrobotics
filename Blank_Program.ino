44e530fcfde1e611c7c5626a1adb2a3698a5d87e
leedeengineer <87783243+leedeengineer@users.noreply.github.com>
2021-08-08 20:18:32 -0400

    Add files via upload

---
+++ Blank_Program.ino
@@ -0,0 +1,27 @@
+/*
+ * Program Name: Blank
+ * Programer:Deengineer
+ * Discription: This is a blank program. This shows what a basic program looks like. 
+ */
+
+ // Comments are parts of the code that the commputer will not read
+ /* 
+  *  Comments are parts of the code that the commputer will not read
+  */
+
+/*
+ * setup() runs once in the program. This is used to initialize your program. 
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+/*
+ * loop() runs continuously in the program as a loop. This is where you will put your
+ * main program. 
+ */
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
